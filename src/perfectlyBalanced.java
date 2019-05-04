db0974b135f4f6336478181380832232c37b2833
bartek <bartek.praselski@outlook.com>
2019-05-04 22:36:36 +0200

    perfectlyBalanced completed

--- src/perfectlyBalanced.java
+++ src/perfectlyBalanced.java
@@ -1,11 +1,35 @@
 
+import java.util.Scanner;
+
+
 public class perfectlyBalanced {
 
-	// user input
 	
-	// array of chars
+	Scanner scan = new Scanner(System.in);
 	
-	// array to count
+	public void checkBalancy() {
 	
-	// result
+	//user input
+		System.out.print("Give a string of characters (only x and Y)");
+		String a = scan.next();
+	
+	//arrays of chars and to count		
+		int[] count = {0,0};
+		char[] chars = a.toCharArray();
+	
+	//counting
+		for(int i:chars) {
+			if(chars[i]=='x') {count[0]+=1;}
+			else {count[1]+=1;}
+		}
+		
+	//result
+		boolean x =false;
+		if(count[0]==count[1]) {x = true;}
+		
+		System.out.print("X = "+ count[0]+"\tY = "+count[1]+"\t "+ x);
+		
+		
+		
+	}
 }
