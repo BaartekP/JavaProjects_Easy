2bf28a73004b1b09d312f1bde6e6bcda28ace005
bartek <bartek.praselski@outlook.com>
2019-05-05 20:40:18 +0200

    beautify update

--- src/b_main.java
+++ src/b_main.java
@@ -4,16 +4,17 @@ public class b_main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			
-		System.out.print("Witaj");	
+		System.out.print("Hello");	
 		
-	//Program 1
+	//Program 1. V1.1 Perfectly Balanced
 		//perfectlyBalanced obj1 = new perfectlyBalanced();
-		
 		//obj1.checkBalancy();
 		
-		perfectlBalancedBonus obj2 = new perfectlBalancedBonus();
+	//Bonus V1.2 Perfectly Balanced Bonus
+		//perfectlBalancedBonus obj2 = new perfectlBalancedBonus();
+		//obj2.checkBalance();
 		
-		obj2.checkBalance();
+	//Program 2. V2.0 Hex colors
 		
 	}
 
